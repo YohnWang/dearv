70a59932b78a167d7f504892311be7cae636e2ce
YohnWang <347220312@qq.com>
2018-11-12 12:21:03 +0800

    fix typo about '>>>'

--- alu.v
+++ alu.v
@@ -14,17 +14,17 @@ module alu
 always@(*)
 begin 
 	case(alusel)
-	`SEL_ADD  : z=x+y;
-	`SEL_SUB  : z=x-y;
-	`SEL_SLL  : z=x<<y;
-	`SEL_SLT  : z=x<y?1:0;
-	`SEL_SLTU : z=$unsigned(x)<$unsigned(y)?1:0;
-	`SEL_XOR  : z=x^y;
-	`SEL_SRL  : z=x>>y;
-	`SEL_SRA  : z=x>>y;
-	`SEL_OR   : z=x|y;
-	`SEL_AND  : z=x&y;
-	default: ;
+		`SEL_ADD  : z=x+y;
+		`SEL_SUB  : z=x-y;
+		`SEL_SLL  : z=x<<y;
+		`SEL_SLT  : z=x<y?1:0;
+		`SEL_SLTU : z=$unsigned(x)<$unsigned(y)?1:0;
+		`SEL_XOR  : z=x^y;
+		`SEL_SRL  : z=x>>y;
+		`SEL_SRA  : z=x>>>y;
+		`SEL_OR   : z=x|y;
+		`SEL_AND  : z=x&y;
+		default: ;
 	endcase
 end
 
